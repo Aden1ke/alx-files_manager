63fd17506d024e39cc919d8284540ffb7ad0281b
israeludofia1990 <udofiaisrael170@gmail.com>
2024-08-04 20:18:39 +0100

    First API

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import AppController from '../controllers/AppController';
+
+const router = express.Router();
+
+// the get Routes
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
